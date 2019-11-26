db4731100697343feccd6043394b6c18bb1ada91
LexingtonCV16 <1145130596@qq.com>
2019-11-26 08:46:39 +0800

    update and do bunch of stuff

--- app/src/main/java/com/example/recyclerviewstudentversion/MyRecyclerAdapter.java
+++ app/src/main/java/com/example/recyclerviewstudentversion/MyRecyclerAdapter.java
@@ -2,8 +2,10 @@ package com.example.recyclerviewstudentversion;
 
 
 import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
 // Todo Implement methods required
 //onCreateViewHolder()
@@ -12,7 +14,23 @@ import androidx.annotation.NonNull;
 public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.MyViewHolder> {
 
 
-// Todo implement ViewHolder
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    // Todo implement ViewHolder
 public static class MyViewHolder extends RecyclerView.ViewHolder {
     // get references to each of the views in the single_item.xml
     // Todo implement constructor
--- app/src/main/java/com/example/recyclerviewstudentversion/Player.java
+++ app/src/main/java/com/example/recyclerviewstudentversion/Player.java
@@ -6,7 +6,7 @@ public class Player{
     int age;
     double worth;
     String main_sport;
-    String url;
+
     int pictureAddress;
     public int getPictureAddress() {
         return pictureAddress;
@@ -16,12 +16,11 @@ public class Player{
         this.pictureAddress = pictureAddress;
     }
 
-    public Player(String name, int age, double worth, String main_sport, String url, int pictureAddress) {
+    public Player(String name, int age, double worth, String main_sport,  int pictureAddress) {
         this.name = name;
         this.age = age;
         this.worth = worth;
         this.main_sport=main_sport;
-        this.url= url;
         this.pictureAddress=pictureAddress;
     }
     public void setName(String name) {
@@ -59,8 +58,6 @@ public class Player{
         return main_sport;
     }
 
-    public String getUrl(){
-        return url;
-    }
+
 
 }
