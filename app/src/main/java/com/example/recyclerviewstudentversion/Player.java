05fed85d4c57957633a9f494ca9ce5fcc6149397
sunny <1390835628@qq.com>
2019-11-27 08:19:53 +0800

    adapter

--- app/src/main/java/com/example/recyclerviewstudentversion/Player.java
+++ app/src/main/java/com/example/recyclerviewstudentversion/Player.java
@@ -1,66 +1,65 @@
-package com.example.recyclerviewstudentversion;
+package com.example.listviewexample;
 
-// Todo create a player class that will hold info about the player
-public class Player{
+import android.widget.ImageView;
+
+public class Player {
+    //Todo finish the class player
+    // Todo 5 member variables
     String name;
     int age;
-    double worth;
+    Long worth;
     String main_sport;
-    String url;
-    int pictureAddress;
-    public int getPictureAddress() {
-        return pictureAddress;
+    int image;
+    String wiki;
+    //Todo name, age, worth(money), main sport and int image resource from drawable
+    //Todo create constructor and getters and setters
+    public Player(String name_I, int age_I, Long worth_I, String main_sport_I, int image_I,String wiki){
+        name = name_I;
+        age = age_I;
+        worth = worth_I;
+        main_sport = main_sport_I;
+        image = image_I;
+        this.wiki=wiki;
     }
 
-    public void setPictureAddress(int pictureAddress) {
-        this.pictureAddress = pictureAddress;
+    public String getName() {
+        return name;
     }
 
-    public Player(String name, int age, double worth, String main_sport, String url, int pictureAddress) {
-        this.name = name;
-        this.age = age;
-        this.worth = worth;
-        this.main_sport=main_sport;
-        this.url= url;
-        this.pictureAddress=pictureAddress;
-    }
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getAge() {
+        return age;
     }
 
     public void setAge(int age) {
         this.age = age;
     }
 
-    public void setWorth(double worth) {
-        this.worth = worth;
-    }
-
-    public void setMain_sport(String main_sport) {
-        this.main_sport = main_sport;
-    }
-
-    public String getName() {
-
-        return name;
-    }
-
-    public int getAge() {
-
-        return age;
-    }
-
-    public double getWorth() {
-
+    public Long getWorth() {
         return worth;
+    }
+
+    public void setWorth(Long worth) {
+        this.worth = worth;
     }
 
     public String getMain_sport() {
         return main_sport;
     }
 
-    public String getUrl(){
-        return url;
+    public void setMain_sport(String main_sport) {
+        this.main_sport = main_sport;
     }
 
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+    // use Android Studio for fast writing code, find Code->Generate
 }
